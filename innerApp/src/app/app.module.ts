64f2e6040f9302656848067aa5f393742f88b848
Pablo Bassil <pabcubus@gmail.com>
2019-01-28 01:51:50 -0500

    commit

--- innerApp/src/app/app.module.ts
+++ innerApp/src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
+import { app_routing } from './app.routes';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './screens/home/home.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    FormsModule,
+    BrowserModule,
+    app_routing
   ],
   providers: [],
   bootstrap: [AppComponent]
